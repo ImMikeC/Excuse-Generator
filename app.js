118acb21a93926e7fe75596ede77152a7a44a097
thinkcentre <thinkcentre@debian-BULLSEYE-live-builder-AMD64>
2022-09-02 18:16:53 -0400

    repositorio de respaldo

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-let sentence = 'This is my car';
-function listado() {
-    return sentence.split(' ');
+/* 
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+} */
+
+let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+
+const whoAleatorio = () => {
+  return Math.floor(Math.random() * who.length)
 }
-console.log(listado()[0])
+const actionAleatorio = () => {
+  return Math.floor(Math.random() * action.length)
+}
+const whatAleatorio = () => {
+  return Math.floor(Math.random() * what.length)
+}
+const whenAleatorio = () => {
+  return Math.floor(Math.random() * when.length)
+}
+
+const fraseAleatoria = () => {
+  return who[whoAleatorio()] + ' ' + action[actionAleatorio()] + ' ' + what[whatAleatorio()] + ' ' + when[whenAleatorio()]
+}
